e3f997f47df419d8c5f6e1a6b3bf67b28e66e3fd
Locochoco <ivanrwpf@gmail.com>
2024-04-14 18:38:55 -0300

    added nix build files

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./nix/shell.nix { inherit pkgs; };
+        }
+      );
+}
---
+++ nix/shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+mkShell {
+  buildInputs = with python311Packages;[
+    python3
+    astroid
+    black
+    click
+    dill
+    isort
+    mccabe
+    mypy-extensions
+    packaging
+    pathspec
+    platformdirs
+    pylint
+    tkinter
+    tomlkit
+  ];
+}
+
